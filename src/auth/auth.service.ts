cd2ce9887f43043b8f1a531516bcdead3858ae65
Stanislav <stasnagaybak@mail.ru>
2023-11-04 09:01:04 +0500

    commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,7 +28,7 @@ export class AuthService {
             throw new HttpException('Пользователь с таким email уже существует', HttpStatus.BAD_REQUEST);
         }
         const hashPassword = await bcrypt.hash(userDto.password, 5);
-        const user =await this.userService.createUser({...userDto, password: hashPassword});
+        const user = await this.userService.createUser({...userDto, password: hashPassword});
         return this.generateToken(user);
     }
 
